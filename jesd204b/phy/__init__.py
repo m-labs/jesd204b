8ed67276faabbe090c3ce302b479fdc73795625e
Florent Kermarrec <florent@enjoy-digital.fr>
2017-04-25 11:21:34 +0200

    phy: more genericity to support others transceivers

--- jesd204b/phy/__init__.py
+++ jesd204b/phy/__init__.py
@@ -5,20 +5,22 @@ from jesd204b.phy.gtx import GTXTransmitter
 
 
 class JESD204BPhyTX(Module):
-    def __init__(self, pll, tx_pads, sys_clk_freq):
+    def __init__(self, pll, tx_pads, sys_clk_freq, transceiver="gtx"):
         self.data = Signal(32)
         self.ctrl = Signal(32//8)
 
         # # #
 
-        # transceiver
-        self.submodules.gtx = GTXTransmitter(
-                pll=pll,
-                tx_pads=tx_pads,
-                sys_clk_freq=sys_clk_freq)
-
+        transmitters = {
+            "gtx": GTXTransmitter
+        }
+        self.submodules.transmitter = transmitters[transceiver](
+            pll=pll,
+            tx_pads=tx_pads,
+            sys_clk_freq=sys_clk_freq
+        )
         for i in range(32//8):
             self.comb += [
-                self.gtx.encoder.d[i].eq(self.data[8*i:8*(i+1)]),
-                self.gtx.encoder.k[i].eq(self.ctrl[i])
+                self.transmitter.encoder.d[i].eq(self.data[8*i:8*(i+1)]),
+                self.transmitter.encoder.k[i].eq(self.ctrl[i])
             ]
